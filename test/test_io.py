400fb0092c0ed3699f8882e51a91d1d1d87290dd
David A. R. Robin <david.a.r.robin@gmail.com>
2019-06-27 09:05:24 +0200

    Add filename parsing tests

--- test/test_io.py
+++ test/test_io.py
@@ -59,3 +59,34 @@ def test_row_metadata_completion():
     assert isinstance(computed_obs, pd.DataFrame)
     assert (computed_obs.columns == expected_obs.columns).all()
     assert (computed_obs == expected_obs).all().all()
+
+def test_get_filename_and_extension():
+    test_cases = [
+            [ "filename.txt", "filename", "txt" ],
+            [ "filename.with.dots.txt", "filename.with.dots", "txt" ],
+            [ "filename with spaces.txt", "filename with spaces", "txt" ],
+            [ "filename.gmt.txt", "filename", "gmt" ],
+            [ "confusing_filename_gmt.txt", "confusing_filename_gmt", "txt" ],
+            [ "filename.grp.txt", "filename", "grp" ],
+            [ "filename.gct.txt", "filename", "gct" ],
+            [ "filename.gmx.txt", "filename", "gmx" ],
+            [ "filename.loom", "filename", "loom" ],
+            ]
+    for filename, basename, ext in test_cases:
+        assert wot.io.get_filename_and_extension(filename) == (basename, ext)
+
+def test_check_file_extension():
+    test_cases = [
+            [ "file.loom", "loom", "file.loom" ],
+            [ "UPCASE.LOOM", "loom", "UPCASE.LOOM" ],
+            [ "wEirdcAse.lOom", "loom", "wEirdcAse.lOom" ],
+            [ "wEirdcAse", "loom", "wEirdcAse.loom" ],
+            [ "file.grp.txt", "grp.txt", "file.grp.txt" ],
+            # FIXME: the behavior of check_file_extension may need to change on these
+            [ "file", "grp", "file.grp" ],
+            [ "file", "grp.txt", "file.grp.txt" ],
+            [ "file.grp", "grp.txt", "file.grp.grp.txt" ],
+            [ "normalcase.loom", "loOm", "normalcase.loom.loOm" ],
+            ]
+    for filename, extension, result in test_cases:
+        assert wot.io.check_file_extension(filename, extension) == result
