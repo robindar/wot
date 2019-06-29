dc485da0bce0fd9dea6a37d63434f8f70fa2d762
David A. R. Robin <david.a.r.robin@gmail.com>
2019-06-30 01:52:38 +0200

    Move dataset metadata test resources to subfolder

--- test/io/hardcoded_small.py
+++ test/io/hardcoded_small.py
@@ -5,3 +5,31 @@ import anndata
 def hardcoded_small_day_pairs():
     data = [ [ 1, 2, 50 ], [ 2, 3, 80 ], [ 1, 3, 10 ] ]
     return pd.DataFrame(data, columns = [ 't0', 't1', 'lambda1' ])
+
+def hardcoded_small_ds():
+    x = np.array([
+        [ 1.0, 2.0, 3.0, 4.0 ],
+        [ 1.2, 2.2, 3.2, 4.2 ],
+        [ 4.3, 3.3, 2.3, 1.3 ],
+        [ 2.4, 3.4, 1.4, 4.4 ],
+        [ 3.5, 1.5, 4.5, 2.5 ]
+        ])
+    obs = pd.DataFrame(index = [ 'cell_1', 'cell_2', 'cell_3', 'cell_4', 'cell_5' ])
+    var = pd.DataFrame(index = [ 'gene_1', 'gene_2', 'gene_3', 'gene_4' ])
+    ds = anndata.AnnData(X=x, obs=obs, var=var)
+    return ds
+
+def hardcoded_small_ds_with_metadata():
+    x = np.array([
+        [ 1.0, 2.0, 3.0, 4.0 ],
+        [ 1.2, 2.2, 3.2, 4.2 ],
+        [ 4.3, 3.3, 2.3, 1.3 ],
+        [ 2.4, 3.4, 1.4, 4.4 ],
+        [ 3.5, 1.5, 4.5, 2.5 ]
+        ])
+    obs = pd.DataFrame([ [ 1, 1., 1 ], [ 2, 2., 1 ], [ 1, 1., 0 ], [ 2, 2., 0 ], [ 1, 1., 1 ]],
+            columns = [ 'day', 'cell_growth_rate', 'covariate' ],
+            index = [ 'cell_1', 'cell_2', 'cell_3', 'cell_4', 'cell_5' ])
+    var = pd.DataFrame(index = [ 'gene_1', 'gene_2', 'gene_3', 'gene_4' ])
+    ds = anndata.AnnData(X=x, obs=obs, var=var)
+    return ds
--- test/io/test_general_io.py
+++ test/io/test_general_io.py
@@ -1,16 +1,6 @@
 from .io_test_helpers import *
 from .hardcoded_small import *
 
-# Sample data generation
-
-def sample_dataset():
-    x = np.array([ [ 1, 2, 3, 0 ], [ 4, 5, 6, 0 ] ])
-    obs = pd.DataFrame(index = [ 'cell_1', 'cell_2' ])
-    var = pd.DataFrame(index = [ 'g1', 'g2', 'g3', 'g4' ])
-    ds = anndata.AnnData(X=x, obs=obs, var=var)
-    return ds
-
-# Tests
 
 def test_read_day_pairs_from_file():
     expected_pairs = hardcoded_small_day_pairs()
@@ -24,28 +14,25 @@ def test_read_day_pairs_from_string():
     assert_pd_frame_equal(read_pairs, expected_pairs)
 
 def test_row_metadata_completion_errors():
-    dataset = sample_dataset()
+    dataset = hardcoded_small_ds()
     for file_param in [ "days", "growth_rates", "covariate" ]:
         kwargs = { file_param: "nonexistent.txt" }
         with pytest.raises(ValueError):
             wot.io.add_row_metadata_to_dataset(dataset, **kwargs)
 
 def test_row_metadata_completion_default_growth_rates():
-    dataset = sample_dataset()
+    dataset = hardcoded_small_ds()
     wot.io.add_row_metadata_to_dataset(dataset)
     assert (dataset.obs['cell_growth_rate'] == 1.).all()
 
 def test_row_metadata_completion():
-    expected_obs = pd.DataFrame([ [ 1, 2., 1 ], [ 2, 3., 0 ] ],
-            columns = [ 'day', 'cell_growth_rate', 'covariate' ],
-            index = [ 'cell_1', 'cell_2' ])
-    dataset = sample_dataset()
+    dataset = hardcoded_small_ds()
     wot.io.add_row_metadata_to_dataset(dataset,
-            days="test/resources/small_days.txt",
-            growth_rates="test/resources/small_growth_rates.txt",
-            covariate="test/resources/small_covariate.txt")
-    computed_obs = dataset.obs
-    assert_pd_frame_equal(computed_obs, expected_obs)
+            days="test/resources/small/days.txt",
+            growth_rates="test/resources/small/growth_rates.txt",
+            covariate="test/resources/small/covariate.txt")
+    expected_dataset = hardcoded_small_ds_with_metadata()
+    assert_anndata_equal(dataset, expected_dataset)
 
 def test_get_filename_and_extension():
     test_cases = [
